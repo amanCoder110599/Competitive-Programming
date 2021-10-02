440a63a35a90496b310f1c2d81664d10af940b7e
Aman Kumar Singh <34673989+amanCoder110599@users.noreply.github.com>
2021-10-02 19:54:27 +0530

    Update Iterative_segment_tree.java

--- Iterative_segment_tree.java
+++ Iterative_segment_tree.java
@@ -1,25 +1,30 @@
-int n;
-    long[] segtree;
-    long[] arr = new long[MAXN];
-    long merge(long x, long y){
-        return x + y;
-    }
-    void build_segtree(){
-        int i = 0;
-        for(i = n - 1; i > 0; i--)
-            segtree[i] = merge(segtree[i << 1], segtree[i << 1 | 1]);
-    }
-    void segtree_update(int ind, long val){
-        for(segtree[ind += n] = val; ind > 1; ind >>= 1)
-            segtree[ind >> 1] = merge(segtree[ind], segtree[ind ^ 1]);
-    }
-    long segtree_query(int l, int r){
-        long res = 0;
-        for(l += n, r += n; l < r; l >>= 1, r >>= 1){
-            if((l & 1) == 1)
-                res = merge(res, segtree[l++]);
-            if((r & 1) == 1)
-                res = merge(res, segtree[--r]);
+class SegtreeMX {
+            int[] segtree;
+            int n;
+            SegtreeMX(int n) {
+                this.n = n;
+                segtree = new int[2 * n];
+            }
+
+            int merge(int x, int y) {
+                return Math.max(x, y);
+            }
+
+            void update(int ind, int val) {
+                ind += n;
+                segtree[ind] = val;
+                for (; ind > 1; ind >>= 1)
+                    segtree[ind >> 1] = merge(segtree[ind], segtree[ind ^ 1]);
+            }
+
+            int query(int l, int r) {
+                int res = 0;
+                for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+                    if ((l & 1) == 1)
+                        res = merge(res, segtree[l++]);
+                    if ((r & 1) == 1)
+                        res = merge(res, segtree[--r]);
+                }
+                return res;
+            }
         }
-        return res;
-    }
